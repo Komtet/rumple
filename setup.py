f7acd2ddc1582af3a96312c8cd94f375c96f9ba7
keipaglows <dark_inko@mail.ru>
2019-01-25 14:30:24 +0300

    version up

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst', 'r') as readme:
 
 setup(
     name='rumple',
-    version='0.1.0',
+    version='0.2.0',
     description='A simple dependency injection container',
     long_description=long_description,
     author='Kilte Leichnam',
